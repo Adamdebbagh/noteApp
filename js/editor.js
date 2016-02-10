ee6aed1782b897210f357865a8f8e08a524227e5
Adam Debbagh <adamdebbagh@gmail.com>
2016-02-10 13:40:13 -0500

    Implement save function to post or update a new note

--- js/editor.js
+++ js/editor.js
@@ -1,8 +1,7 @@
 angular.module('noteApp')
 .controller('EditorController', function($scope,$http){
 
-    $scope.state = { editing: true };
-
+    $scope.editing = true;
 
     $http.get('/notes')
         .success(function (data) {
@@ -23,5 +22,30 @@ angular.module('noteApp')
             title:'',
             content:''
         };
+    };
+
+    $scope.save = function () {
+        $scope.content.date = new Date();
+
+        if ($scope.content.id) {
+            $http.put('/notes/' + $scope.content.id, $scope.content).success(function (data) {
+                $scope.editing = false;
+            }).error(function (err) {
+                $scope.error = 'Could not update note';
+            });
+        } else {
+            $scope.content.id = Date.now();
+            $http.post('/notes', $scope.content).success(function (data) {
+                $scope.notes.push($scope.content);
+                $scope.editing = false;
+            }).error(function (err) {
+                $scope.error = 'Could not create note';
+            });
+        }
+    };
+
+
+    $scope.delete = function(){
+
     };
 });
