4bbd6dd2d55c62d2e9c25ef18128a10295d22980
Adam Debbagh <adamdebbagh@gmail.com>
2016-02-10 11:28:23 -0500

    Added markdown to content

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-angular.module('noteApp', []);
+angular.module('noteApp', [])
+    .directive('markdown',function(){
+        var converter = new Showdown.converter();
+        return {
+            scope: {
+                markdown: '@'
+            },
+            link: function (scope, element, attrs) {
+                scope.$watch('markdown', function () {
+                    var content = converter.makeHtml(attrs.markdown);
+                    element.html(content);
+                });
+            }
+        }
+    })
+;
